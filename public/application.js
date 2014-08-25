108e7424990c6bfe10e8e4690de66f00c00b7232
chischaschos <larin.s931@gmail.com>
2014-08-24 23:21:20 -0500

    Extract graph creation duplicated code to function

--- public/application.js
+++ public/application.js
@@ -1,83 +1,98 @@
-d3.json('/api/totals/monthly', function(error, json) {
+var Graph = function(data) {
   var height = 200;
   var width = 400;
   var barPadding = 5;
   var yPadding = 10;
   var xPadding = 20;
 
+  var svg = d3.select('body').append('svg').
+    attr({
+      height: height,
+      width: width
+    });
+  var barWidth = (width - 2 * xPadding) / data.length - barPadding;
+
+  var yScale = d3.scale.linear().
+    domain([0, d3.max(data, function(datum) { return datum.to_pay_total; })]).
+    range([height - yPadding, yPadding]);
+
+  var xScale = d3.scale.linear().
+    domain([0, data.length]).
+    range([xPadding, width - xPadding]);
+
+  var yAxis = d3.svg.axis().
+    scale(yScale).
+    orient('left');
+
+  var xAxis = d3.svg.axis().
+    scale(xScale).
+    orient('bottom');
+
+  svg.selectAll('rect').
+    data(data).
+    enter().
+    append('rect').
+    attr({
+      x: function(datum, index) {
+        return xScale(index) + xPadding;
+      },
+      y: function(datum) {
+        return height - yPadding * 2 - (height - yScale(datum.to_pay_total) - yPadding) + 10;
+      },
+      width: function() {
+        return barWidth;
+      },
+      height: function(datum) {
+        return height - yScale(datum.to_pay_total) - yPadding - 10;
+      },
+      fill: function(datum) {
+        return "rgb(0, 0, 0)";
+      }
+    });
+
+  svg.selectAll('text').
+    data(data).
+    enter().
+    append('text').
+    classed('totalsLabel', true).
+    text(function(datum) {
+      return datum.to_pay_total;
+    }).
+    attr({
+      x: function(datum, index) {
+        return xScale(index) + xPadding + barWidth / 2;
+      },
+      y: function(datum) {
+        return height - yPadding * 2 - (height - yScale(datum.to_pay_total) - yPadding) + 25;
+      }
+    });
+
+  svg.append('g').attr({
+    class: 'axis',
+    transform: "translate(" + (xPadding * 2) + ", -" + yPadding +")"
+  }).call(yAxis);
+  svg.append('g').attr({
+    class: 'axis',
+    transform: "translate(" + xPadding + ", " + (height - yPadding * 2) + ")"
+  }).call(xAxis);
+}
+
+d3.json('/api/totals/monthly', function(error, json) {
   for (var agency in json) {
-    var svg = d3.select('body').append('svg').
-      attr({
-        height: height,
-        width: width
-      });
-    var barWidth = (width - 2 * xPadding) / json[agency].length - barPadding;
-
-    var yScale = d3.scale.linear().
-      domain([0, d3.max(json[agency], function(datum) { return datum.to_pay_total; })]).
-      range([height - yPadding, yPadding]);
-
-    var xScale = d3.scale.linear().
-      domain([0, json[agency].length]).
-      range([xPadding, width - xPadding]);
-
-    var yAxis = d3.svg.axis().
-      scale(yScale).
-      orient('left');
-
-    var xAxis = d3.svg.axis().
-      scale(xScale).
-      orient('bottom');
-
-    svg.selectAll('rect').
-      data(json[agency]).
-      enter().
-      append('rect').
-      attr({
-        x: function(datum, index) {
-          return xScale(index) + xPadding;
-        },
-        y: function(datum) {
-          return height - yPadding * 2 - (height - yScale(datum.to_pay_total) - yPadding) + 10;
-        },
-        width: function() {
-          return barWidth;
-        },
-        height: function(datum) {
-          return height - yScale(datum.to_pay_total) - yPadding - 10;
-        },
-        fill: function(datum) {
-          return "rgb(0, 0, 0)";
-        }
-      });
-
-    svg.selectAll('text').
-      data(json[agency]).
-      enter().
-      append('text').
-      classed('totalsLabel', true).
-      text(function(datum) {
-        return datum.to_pay_total;
-      }).
-      attr({
-        x: function(datum, index) {
-          return xScale(index) + xPadding + barWidth / 2;
-        },
-        y: function(datum) {
-          return height - yPadding * 2 - (height - yScale(datum.to_pay_total) - yPadding) + 25;
-        }
-      });
-
-    svg.append('g').attr({
-      class: 'axis',
-      transform: "translate(" + (xPadding * 2) + ", -" + yPadding +")"
-    }).call(yAxis);
-    svg.append('g').attr({
-      class: 'axis',
-      transform: "translate(" + xPadding + ", " + (height - yPadding * 2) + ")"
-    }).call(xAxis);
+    Graph(json[agency]);
   }
 
+  var accumulatedNumbers = [];
+
+  for (var agency in json) {
+    json[agency].forEach(function(fields, index) {
+      accumulatedNumbers[index] = accumulatedNumbers[index] || {date: fields.date, to_pay_total: 0, commission: 0};
+      accumulatedNumbers[index].to_pay_total += fields.to_pay_total;
+      accumulatedNumbers[index].commission += fields.commission;
+    });
+  };
+
+  Graph(accumulatedNumbers);
 });
 
 d3.json('/api/days_status', function(error, json) {
